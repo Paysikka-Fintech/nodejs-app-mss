57476639c2114369a8665efce55c02f4dd8eb94e
Amulyakusuma <112145354+Amulyakusuma@users.noreply.github.com>
2023-02-01 14:51:51 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-#_Change_Working_Directory
-cd /home/ubuntu/server
-
-#_Update_&_Set_Node_Version
-curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-
-#_Download_Node_&NPM
-sudo apt-get install -y nodejs
-
-#_Download_PM2
-npm install pm2@latest -g
-
-#_Download_Nginx
-apt-get install nginx -y
+APP_DIR=/var/www/html/application
