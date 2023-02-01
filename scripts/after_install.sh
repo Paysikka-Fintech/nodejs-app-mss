48ce9f75ca36ad4567015a98296d16b9c90f6081
Amulyakusuma <112145354+Amulyakusuma@users.noreply.github.com>
2023-02-01 14:51:28 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 
-#_Change_Working_Directory
-cd /home/ubuntu/server
+APP_DIR=/var/www/html/application
+HOSTNAME=__SERVER_HOSTNAME__
+APP_VERSION=__VERSION__
+ENV=__ENV__
 
-#_Remove_Unused_Code
-rm -rf node_modules
-rm -rf build
+#########################################
+######### LARAVEL CONFIGURATION #########
+#########################################
 
-#Install_node_modules_&_Make_React_Build
-npm  install
-npm run build
+chmod -R 777 $APP_DIR/storage
+chmod -R 777 $APP_DIR/bootstrap/cache
+
+#########################################
+############### Build ENV ###############
+#########################################
+
+cp $APP_DIR/.github/deployment/server/.env $APP_DIR/.env
+sed -i "s/__VERSION__/$VERSION/g" $APP_DIR/.env
+sh $APP_DIR/.github/deployment/server/AwsSecrets.sh $ENV/$HOSTNAME $APP_DIR/.env
+
+cd $APP_DIR && php artisan optimize && php artisan config:clear
+
+#cd $APP_DIR && php artisan config:clear | fails if this is enabled
+cd $APP_DIR && php artisan route:cache
+cd $APP_DIR && php artisan view:cache
+
+##### Migration
+cd $APP_DIR && php artisan migrate --force
+
+#########################################
+############# NGNIX SERVER ##############
+#########################################
+rm -rf /etc/nginx/sites-available/default
+rm -rf /etc/nginx/sites-enabled/default
+cp $APP_DIR/.github/deployment/server/laravel.conf /etc/nginx/conf.d/laravel.conf
