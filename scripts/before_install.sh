e8a416aec018eb97e38e55591bbdb90e1eb9ff51
Amulyakusuma <112145354+Amulyakusuma@users.noreply.github.com>
2023-02-01 11:42:28 +0530

    Create before_install.sh

---
+++ scripts/before_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#_Change_Working_Directory
+cd /home/ubuntu/server
+
+#_Update_&_Set_Node_Version
+curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+
+#_Download_Node_&NPM
+sudo apt-get install -y nodejs
+
+#_Download_PM2
+npm install pm2@latest -g
+
+#_Download_Nginx
+apt-get -y install nginx
