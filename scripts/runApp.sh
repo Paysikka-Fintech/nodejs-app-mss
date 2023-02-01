8a8d1c059ed1fa5ac31907e0bfc09d7d6827072a
Amulyakusuma <112145354+Amulyakusuma@users.noreply.github.com>
2023-02-01 12:56:58 +0530

    Update runApp.sh

--- scripts/runApp.sh
+++ scripts/runApp.sh
@@ -3,13 +3,5 @@
 #_Change_Working_Directory
 cd /home/ubuntu/server
 
-echo 'The following "npm" command runs your Node.js application'
-
-set -x
-npm start &
-sleep 1
-echo $! > .pidfile
-set +x
-
-echo 'Now you can'
-echo 'Visit http://ServerIp:9981 to see your Node.js application'
+#sudo pm2 start server.js --watch
+pm2 start server.js --watch
